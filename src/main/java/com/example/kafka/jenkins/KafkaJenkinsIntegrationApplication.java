b58a4f8e964f538e32c427d7e95cc584e3aa64ba
Arun Kumar <arun.k.datika46@gmail.com>
2022-07-21 12:20:41 +0530

    Pom Update

---
+++ src/main/java/com/example/kafka/jenkins/KafkaJenkinsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaJenkinsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaJenkinsIntegrationApplication.class, args);
+	}
+
+}
